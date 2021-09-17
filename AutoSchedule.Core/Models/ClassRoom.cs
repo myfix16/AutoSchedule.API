53356bb8b9ba961db6f835c2f02c03f3dc05e395
Frank Mao <61649477+myfix16@users.noreply.github.com>
2021-09-17 21:30:25 +0800

    temp

---
+++ AutoSchedule.Core/Models/ClassRoom.cs
@@ -0,0 +1,21 @@
+﻿namespace AutoSchedule.Core.Models
+{
+    public enum ClassRoom
+    {
+        ChengDao,
+        ZhiXin,
+        Zhiren,
+        TD,
+        TC,
+        TB,
+        TA,
+        /// <summary>
+        /// Administration Building East block
+        /// </summary>
+        ABE,
+        /// <summary>
+        /// Administration Building West block
+        /// </summary>
+        ABW,
+    }
+}
